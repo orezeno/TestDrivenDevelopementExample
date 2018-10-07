86e5a7575c11d23f07957ec9f26064c3042ebe84
2466799300@qq.com <2466799300@qq.com>
2018-10-07 10:34:24 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,19 +16,35 @@ class NewVisitorTest(unittest.TestCase):
     def test_start_a_list_and_retrieve_it_later(self):
         # 打开应用首页
         self.browser.get('http://localhost:8000/')
+
         # 应用网页标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 应用邀请用户输入待办事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # 用户在文本框中输入“Buy peacock feathers”
+        inputbox.send_keys('Buy peacock feathers')
 
         # 用户按回车键后，页面更新了
         # 待办事项列表中显示了“1：Buy peacock feathers”
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(rows.text == '1：Buy peacock feathers' for row in rows)
+        )
 
         # 页面中显示一个文本框，可以输入其他的待办事项
         # 用户输入了“Use peacock feathers to make a fly”
+        self.fail('Finish the test!')
 
         # 页面再次更新，清单中显示这两个待办事项
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,19 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
+from django.http import HttpRequest
 
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
