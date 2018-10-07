6af5045d1cf52c469d0314b4e0bfa3de1e7c081b
2466799300@qq.com <2466799300@qq.com>
2018-10-07 17:10:00 +0800

    Model for list Item and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_a_list_and_retrieve_it_later(self):
         # 打开应用首页
         self.browser.get('http://localhost:8000/')
@@ -36,22 +41,19 @@ class NewVisitorTest(unittest.TestCase):
         # 待办事项列表中显示了“1：Buy peacock feathers”
         inputbox.send_keys(Keys.ENTER)
 
-        import time
-        time.sleep(5)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1:Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table -- its text was:\n%s' % (
-                table.text
-            )
-        )
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         # 页面中显示一个文本框，可以输入其他的待办事项
         # 用户输入了“Use peacock feathers to make a fly”
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
+        import time
+        time.sleep(5)
         # 页面再次更新，清单中显示这两个待办事项
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
 
         # 验证网站是否会记住用户的清单
         # 用户看到网站为他生成了一个唯一的URL
--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from lists.views import home_page
 from django.http import HttpRequest
 from django.shortcuts import render_to_response
+from lists.models import Item
 
 
 # Create your tests here.
@@ -37,3 +38,23 @@ class HomePageTest(TestCase):
         #     {'new_item_text': 'A new list item'}
         # )
         # self.assertEqual(response.content.decode(), expected_page.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_item = Item.objects.all()
+        self.assertEqual(saved_item.count(), 2)
+
+        first_saved_item = saved_item[0]
+        second_saved_item = saved_item[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
